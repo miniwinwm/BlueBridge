195cb3a25abf4297aef5700c69dfa3e9f91dcca9
John Blaiklock <johnblaiklock@hotmail.com>
2022-04-19 17:55:31 +0100

    Added dataflow diagram

--- main/spp_acceptor.c
+++ main/spp_acceptor.c
@@ -22,6 +22,27 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+This work is based on code in file BluetoothSerial.cpp distributed as part of 
+the ESP32 driver layer of the Arduino library for the ESP32 platform. The 
+license for the source code is given below:
+
+// Copyright 2018 Evandro Luis Copercini
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License. 
+
+The above source code has been modified to enable it to build directly 
+with the ESP32-IDF framework.
+
 */
 
 /***************
